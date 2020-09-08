abc4dbd6399f61ef6dee59539437bce5961d4130
Duy <huynhduytam2210@gmail.com>
2020-09-08 23:26:21 +0700

    Creating Comments

--- comments/index.js
+++ comments/index.js
@@ -16,10 +16,10 @@ app.get('/posts/:id/comments', (req, res) => {
 app.post('/posts/:id/comments', (req, res) => {
     const commentId = randomBytes(4).toString('hex')
     const { content } = req.body
-    const comments = commentsByPostId[req.params.id]
+    const comments = commentsByPostId[req.params.id] || []
     comments.push({ id: commentId, content })
     commentsByPostId[req.params.id] = comments
-    res.send(comments).status(401)
+    res.send(comments).status(201)
 })
 
 app.listen(4001, () => {
