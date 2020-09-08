8c3b09e9c5b52c6e4852f33865d1f4d066a33308
Duy <huynhduytam2210@gmail.com>
2020-09-09 00:27:58 +0700

    Emitting Comment Creation Events

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 const app = express()
 
 app.use(bodyParser.json())
@@ -13,13 +14,27 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || [])
 })
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex')
     const { content } = req.body
     const comments = commentsByPostId[req.params.id] || []
     comments.push({ id: commentId, content })
     commentsByPostId[req.params.id] = comments
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    })
+
     res.send(comments).status(201)
+})
+
+app.post('/events', async (req, res) => {
+    console.log('Receive event comments')
 })
 
 app.listen(4001, () => {
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const { randomBytes } = require('crypto')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 const cors = require('cors')
 app.use(bodyParser.json())
 app.use(cors())
@@ -12,7 +13,7 @@ app.get('/posts', (req, res) => {
     res.send(posts)
 })
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex')
     const { title } = req.body
 
@@ -21,7 +22,19 @@ app.post('/posts', (req, res) => {
         title
     }
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id,
+            title
+        }
+    })
+
     res.status(201).send(posts[id])
+})
+
+app.post('/events', async (req, res) => {
+    console.log('Receive event posts')
 })
 
 app.listen(4000, () => {
