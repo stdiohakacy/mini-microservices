7919d95e1c7cb834ca7a39eb9b567e7cfe97f0e4
Duy <huynhduytam2210@gmail.com>
2020-09-08 17:22:22 +0700

    initial project

---
+++ comments/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
+const app = express()
+
+app.use(bodyParser.json())
+
+const commentsByPostId = {}
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || [])
+})
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex')
+    const { content } = req.body
+    const comments = commentsByPostId[req.params.id]
+    comments.push({ id: commentId, content })
+    commentsByPostId[req.params.id] = comments
+    res.send(comments).status(401)
+})
+
+app.listen(4001, () => {
+    console.log(`Server listening on port 4001`)
+})
