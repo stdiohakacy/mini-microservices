824c9e004eff7fefbdc7a774d96e5a30f73ec115
Duy <huynhduytam2210@gmail.com>
2020-09-09 11:56:57 +0700

    Event Syncing in Action

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,11 +4,19 @@ const axios = require('axios')
 const cors = require('cors')
 const app = express()
 
+const events = []
+
 app.use(bodyParser.json())
 app.use(cors())
 
+app.get('/events', (rqe, res) => {
+    res.send(events).status(200)
+})
+
 app.post('/events', (req, res) => {
     const event = req.body
+
+    events.push(event)
 
     axios.post('http://localhost:4000/events', event)
     axios.post('http://localhost:4001/events', event)
--- query/index.js
+++ query/index.js
@@ -1,20 +1,14 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
 const posts = {}
-
-app.get('/posts', (req, res) => {
-    res.send(posts)
-})
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body
-
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data
         posts[id] = { id, title, comments: [] }
@@ -34,11 +28,25 @@ app.post('/events', (req, res) => {
         comment.status = status
         comment.content = content
     }
+}
 
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body
+    handleEvent(type, data)
     res.send({})
 })
 
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Server listening on port 4002')
+    const res = await axios.get('http://localhost:4005/events')
+
+    for (const event of res.data) {
+        console.log(`Processing event ${event.type}`)
+        handleEvent(event.type, event.data)
+    }
 })
