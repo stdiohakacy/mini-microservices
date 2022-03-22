0aa2c0756d53481b68c363abae93e3d16380b567
Cornal <nguyendangduy2210@gmail.com>
2022-03-22 17:03:39 +0700

    [flag] - can not call to event bus service

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,7 +12,7 @@ app.post('/events', (req, res) => {
     events.push(event);
 
     axios.post('http://posts-clusterip-srv:4000/events', event);
-    axios.post('http://comment-srv:4001/events', event);
+    axios.post('http://comments-srv:4001/events', event);
     axios.post('http://query-srv:4002/events', event);
     axios.post('http://moderation-srv:4003/events', event);
 
